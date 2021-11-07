dfec1c2ef0b5420f0e823c59db9ca94a17eb8dae
Alexey Dolgiy <alexey.dolgiy@gmail.com>
2021-11-07 23:47:04 +0200

    task 01 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
-{}
+const startButton = document.querySelector('button[data-start]');
+console.log(startButton);
+
+const stopButton = document.querySelector('button[data-stop]');
+console.log(stopButton);
+
+let timerId = null;
+
+startButton.addEventListener('click', onStartButtonClick);
+
+function onStartButtonClick() {
+
+    timerId = setInterval(() => {
+        function getRandomHexColor() {
+            return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+          }
+            
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+
+    startButton.setAttribute('disabled', '')
+    
+};
+
+stopButton.addEventListener('click', onStopButtonClick);
+
+function onStopButtonClick() {
+    clearInterval(timerId);
+    startButton.removeAttribute('disabled', '')
+
+}
