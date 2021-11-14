55e3c9c4a53094b50b27cb51d1396a90f34ac33e
Alexey Dolgiy <alexey.dolgiy@gmail.com>
2021-11-14 21:31:15 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -31,12 +31,15 @@ function onSubmitClick(evt) {
   createPromise();
 }
 
-
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+for( let i = 0; i <= amount; i += 1) {
+  function createPromise(position, delay) {
+    const shouldResolve = Math.random() > 0.3;
+    if (shouldResolve) {
+      // Fulfill
+    } else {
+      // Reject
+    }
   }
+
 }
+
